7b6af093e6185d951ba210304870d95d804329e7
Dominique Vernier <dvernier@redhat.com>
2020-12-08 08:55:37 -0500

    remove manager from test (#183)
    * remove manager from test and Add version in ignore sonar

--- pkg/controller/managedcluster/managedcluster_controller.go
+++ pkg/controller/managedcluster/managedcluster_controller.go
@@ -10,20 +10,15 @@ import (
 	"time"
 
 	corev1 "k8s.io/api/core/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/event"
-	"sigs.k8s.io/controller-runtime/pkg/handler"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-	"sigs.k8s.io/controller-runtime/pkg/source"
 
 	clusterv1 "github.com/open-cluster-management/api/cluster/v1"
 	workv1 "github.com/open-cluster-management/api/work/v1"
@@ -52,18 +47,6 @@ var log = logf.Log.WithName("controller_managedcluster")
 * business logic.  Delete these comments after modifying this file.*
  */
 
-// Add creates a new ManagedCluster Controller and adds it to the Manager. The Manager will set fields on the Controller
-// and Start it when the Manager is Started.
-func Add(mgr manager.Manager) error {
-	return add(mgr, newReconciler(mgr))
-}
-
-// newReconciler returns a new reconcile.Reconciler
-func newReconciler(mgr manager.Manager) reconcile.Reconciler {
-	client := newCustomClient(mgr.GetClient(), mgr.GetAPIReader())
-	return &ReconcileManagedCluster{client: client, scheme: mgr.GetScheme()}
-}
-
 // customClient will do get secret without cache, other operations are like normal cache client
 type customClient struct {
 	client.Client
@@ -83,107 +66,6 @@ func (cc customClient) Get(ctx context.Context, key client.ObjectKey, obj runtim
 		return cc.APIReader.Get(ctx, key, obj)
 	}
 	return cc.Client.Get(ctx, key, obj)
-}
-
-// add adds a new Controller to mgr with r as the reconcile.Reconciler
-func add(mgr manager.Manager, r reconcile.Reconciler) error {
-	// Create a new controller
-	c, err := controller.New("managedcluster-controller", mgr, controller.Options{Reconciler: r})
-	if err != nil {
-		return err
-	}
-
-	// Watch for changes to primary resource ManagedCluster
-	err = c.Watch(
-		&source.Kind{Type: &clusterv1.ManagedCluster{}},
-		&handler.EnqueueRequestForObject{},
-	)
-	if err != nil {
-		return err
-	}
-
-	// Watch for changes to secondary resource Pods and requeue the owner ManagedCluster
-	err = c.Watch(
-		&source.Kind{Type: &rbacv1.ClusterRole{}},
-		&handler.EnqueueRequestForOwner{
-			IsController: true,
-			OwnerType:    &clusterv1.ManagedCluster{},
-		},
-	)
-	if err != nil {
-		log.Error(err, "Fail to add Watch for ClusterRole to controller")
-		return err
-	}
-
-	err = c.Watch(
-		&source.Kind{Type: &corev1.ServiceAccount{}},
-		&handler.EnqueueRequestForOwner{
-			IsController: true,
-			OwnerType:    &clusterv1.ManagedCluster{},
-		},
-	)
-	if err != nil {
-		log.Error(err, "Fail to add Watch for ServiceAccount to controller")
-		return err
-	}
-
-	err = c.Watch(
-		&source.Kind{Type: &hivev1.ClusterDeployment{}},
-		&handler.EnqueueRequestsFromMapFunc{
-			ToRequests: handler.ToRequestsFunc(func(obj handler.MapObject) []reconcile.Request {
-				return []reconcile.Request{
-					{
-						NamespacedName: types.NamespacedName{
-							Name:      obj.Meta.GetName(),
-							Namespace: obj.Meta.GetNamespace(),
-						},
-					},
-				}
-			}),
-		},
-	)
-	if err != nil {
-		log.Error(err, "Fail to add Watch for ClusterDeployment to controller")
-		return err
-	}
-
-	err = c.Watch(
-		&source.Kind{Type: &rbacv1.ClusterRoleBinding{}},
-		&handler.EnqueueRequestForOwner{
-			IsController: true,
-			OwnerType:    &clusterv1.ManagedCluster{},
-		},
-	)
-	if err != nil {
-		log.Error(err, "Fail to add Watch for ClusterRoleBinding to controller")
-		return err
-	}
-
-	err = c.Watch(
-		&source.Kind{Type: &hivev1.SyncSet{}},
-		&handler.EnqueueRequestForOwner{
-			IsController: true,
-			OwnerType:    &clusterv1.ManagedCluster{},
-		},
-	)
-	if err != nil {
-		log.Error(err, "Fail to add Watch for SyncSet to controller")
-		return err
-	}
-
-	err = c.Watch(
-		&source.Kind{Type: &workv1.ManifestWork{}},
-		&handler.EnqueueRequestForOwner{
-			IsController: true,
-			OwnerType:    &clusterv1.ManagedCluster{},
-		},
-		newManifestWorkSpecPredicate(),
-	)
-	if err != nil {
-		log.Error(err, "Fail to add Watch for ManifestWork to controller")
-		return err
-	}
-	return nil
 }
 
 func newManifestWorkSpecPredicate() predicate.Predicate {
