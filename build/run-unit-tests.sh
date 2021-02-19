6d56d3b293a9113b523ce2f91dc24f6249fb4341
Han Zhang <hanzhang@redhat.com>
2021-02-19 10:54:23 -0500

    merge prow updates (#6)
    * remove user not in openshift org
    
    * add prow makefile & dockerfile
    
    * update makefile (#4)
    
    * simplify dockerfile.prow (#5)

--- build/run-unit-tests.sh
+++ build/run-unit-tests.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -e
 
 _script_dir=$(dirname "$0")
+if ! which gocovmerge > /dev/null; then  echo "Installing gocovmerge..."; go get -u github.com/wadey/gocovmerge; fi
+if ! which patter > /dev/null; then      echo "Installing patter ..."; go get -u github.com/apg/patter; fi
+
+export GOFLAGS=""
 mkdir -p test/unit/coverage
 echo 'mode: atomic' > test/unit/coverage/cover.out
 echo '' > test/unit/coverage/cover.tmp
