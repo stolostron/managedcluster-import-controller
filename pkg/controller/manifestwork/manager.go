7de5b621cbcf0c2962d5097addaf08a4dfe48932
Wei Liu <skeeey.liu@gmail.com>
2021-09-10 10:29:12 +0800

    add manifestwork-controller (#79)
    Signed-off-by: liuwei <liuweixa@redhat.com>

---
+++ pkg/controller/manifestwork/manager.go
@@ -0,0 +1,132 @@
+// Copyright (c) Red Hat, Inc.
+// Copyright Contributors to the Open Cluster Management project
+
+package manifestwork
+
+import (
+	"strings"
+
+	clusterv1 "github.com/open-cluster-management/api/cluster/v1"
+	workv1 "github.com/open-cluster-management/api/work/v1"
+	"github.com/open-cluster-management/managedcluster-import-controller/pkg/constants"
+	"github.com/open-cluster-management/managedcluster-import-controller/pkg/helpers"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
+
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+const controllerName = "manifestwork-controller"
+
+const (
+	klusterletSuffix     = "klusterlet"
+	klusterletCRDsSuffix = "klusterlet-crds"
+)
+
+// Add creates a new manifestwork controller and adds it to the Manager.
+// The Manager will set fields on the Controller and Start it when the Manager is Started.
+func Add(mgr manager.Manager, clientHolder *helpers.ClientHolder) (string, error) {
+	return controllerName, add(mgr, newReconciler(mgr, clientHolder))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager, clientHolder *helpers.ClientHolder) reconcile.Reconciler {
+	return &ReconcileManifestWork{
+		clientHolder: clientHolder,
+		scheme:       mgr.GetScheme(),
+		recorder:     helpers.NewEventRecorder(clientHolder.KubeClient, controllerName),
+	}
+}
+
+// adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	c, err := controller.New(controllerName, mgr, controller.Options{
+		Reconciler:              r,
+		MaxConcurrentReconciles: helpers.GetMaxConcurrentReconciles(),
+	})
+	if err != nil {
+		return err
+	}
+
+	if err := c.Watch(
+		&source.Kind{Type: &workv1.ManifestWork{}},
+		// handler.EnqueueRequestsFromMapFunc(func(o client.Object) []reconcile.Request {
+		// 	return []reconcile.Request{
+		// 		{
+		// 			NamespacedName: types.NamespacedName{
+		// 				Name: o.GetNamespace(),
+		// 			},
+		// 		},
+		// 	}
+		// }),
+		nil,
+		predicate.Predicate(predicate.Funcs{
+			GenericFunc: func(e event.GenericEvent) bool { return false },
+			CreateFunc:  func(e event.CreateEvent) bool { return true },
+			DeleteFunc:  func(e event.DeleteEvent) bool { return true },
+			UpdateFunc: func(e event.UpdateEvent) bool {
+				workName := e.MetaNew.GetName()
+				// for update event, only watch klusterlet manifest works
+				if !strings.HasSuffix(workName, klusterletCRDsSuffix) &&
+					!strings.HasSuffix(workName, klusterletSuffix) {
+					return false
+				}
+
+				new, okNew := e.ObjectNew.(*workv1.ManifestWork)
+				old, okOld := e.ObjectOld.(*workv1.ManifestWork)
+				if okNew && okOld {
+					return !helpers.ManifestsEqual(new.Spec.Workload.Manifests, old.Spec.Workload.Manifests)
+				}
+
+				return false
+			},
+		}),
+	); err != nil {
+		return err
+	}
+
+	if err := c.Watch(
+		&source.Kind{Type: &clusterv1.ManagedCluster{}},
+		&handler.EnqueueRequestForObject{},
+	); err != nil {
+		return err
+	}
+
+	if err := c.Watch(
+		&source.Kind{Type: &corev1.Secret{}},
+		&handler.EnqueueRequestForOwner{
+			IsController: true,
+			OwnerType:    &clusterv1.ManagedCluster{},
+		},
+		predicate.Predicate(predicate.Funcs{
+			GenericFunc: func(e event.GenericEvent) bool { return false },
+			DeleteFunc:  func(e event.DeleteEvent) bool { return false },
+			CreateFunc:  func(e event.CreateEvent) bool { return false },
+			UpdateFunc: func(e event.UpdateEvent) bool {
+				secretName := e.MetaNew.GetName()
+				// only handles the import secret changes
+				if !strings.HasSuffix(secretName, constants.ImportSecretNameSuffix) {
+					return false
+				}
+
+				new, okNew := e.ObjectNew.(*corev1.Secret)
+				old, okOld := e.ObjectOld.(*corev1.Secret)
+				if okNew && okOld {
+					return !equality.Semantic.DeepEqual(old.Data, new.Data)
+				}
+
+				return false
+			},
+		}),
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ pkg/controller/manifestwork/manifestwork_controller_test.go
@@ -0,0 +1,291 @@
+// Copyright (c) Red Hat, Inc.
+// Copyright Contributors to the Open Cluster Management project
+
+package manifestwork
+
+import (
+	"context"
+	"testing"
+
+	clusterv1 "github.com/open-cluster-management/api/cluster/v1"
+	workv1 "github.com/open-cluster-management/api/work/v1"
+	"github.com/open-cluster-management/managedcluster-import-controller/pkg/helpers"
+	testinghelpers "github.com/open-cluster-management/managedcluster-import-controller/pkg/helpers/testing"
+
+	"github.com/openshift/library-go/pkg/operator/events/eventstesting"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+var (
+	testscheme = scheme.Scheme
+	now        = v1.Now()
+)
+
+func init() {
+	testscheme.AddKnownTypes(clusterv1.SchemeGroupVersion, &clusterv1.ManagedCluster{})
+	testscheme.AddKnownTypes(workv1.SchemeGroupVersion, &workv1.ManifestWork{})
+	testscheme.AddKnownTypes(workv1.SchemeGroupVersion, &workv1.ManifestWorkList{})
+}
+
+func TestReconcile(t *testing.T) {
+	cases := []struct {
+		name         string
+		startObjs    []runtime.Object
+		request      reconcile.Request
+		validateFunc func(t *testing.T, runtimeClient client.Client)
+	}{
+		{
+			name:      "no managed clusters",
+			startObjs: []runtime.Object{},
+			request: reconcile.Request{
+				NamespacedName: types.NamespacedName{
+					Name: "test",
+				},
+			},
+			validateFunc: func(t *testing.T, runtimeClient client.Client) {
+				// do nothing
+			},
+		},
+		{
+			name: "no manifest works",
+			startObjs: []runtime.Object{
+				&clusterv1.ManagedCluster{
+					ObjectMeta: v1.ObjectMeta{
+						Name:       "test",
+						Finalizers: []string{manifestWorkFinalizer},
+					},
+				},
+			},
+			request: reconcile.Request{
+				NamespacedName: types.NamespacedName{
+					Name: "test",
+				},
+			},
+			validateFunc: func(t *testing.T, runtimeClient client.Client) {
+				managedCluster := &clusterv1.ManagedCluster{}
+				if err := runtimeClient.Get(context.TODO(), types.NamespacedName{Name: "test"}, managedCluster); err != nil {
+					t.Errorf("unexpected error: %v", err)
+				}
+				if len(managedCluster.Finalizers) != 0 {
+					t.Errorf("expected no finalizers, but failed")
+				}
+			},
+		},
+		{
+			name: "manifest works are created",
+			startObjs: []runtime.Object{
+				&clusterv1.ManagedCluster{
+					ObjectMeta: v1.ObjectMeta{
+						Name: "test",
+					},
+				},
+				&workv1.ManifestWork{
+					ObjectMeta: v1.ObjectMeta{
+						Name:      "test",
+						Namespace: "test",
+					},
+				},
+			},
+			request: reconcile.Request{
+				NamespacedName: types.NamespacedName{
+					Name: "test",
+				},
+			},
+			validateFunc: func(t *testing.T, runtimeClient client.Client) {
+				managedCluster := &clusterv1.ManagedCluster{}
+				if err := runtimeClient.Get(context.TODO(), types.NamespacedName{Name: "test"}, managedCluster); err != nil {
+					t.Errorf("unexpected error: %v", err)
+				}
+				if len(managedCluster.Finalizers) != 1 {
+					t.Errorf("expected one finalizer, but failed")
+				}
+			},
+		},
+		{
+			name: "managed clusters is deleting",
+			startObjs: []runtime.Object{
+				&clusterv1.ManagedCluster{
+					ObjectMeta: v1.ObjectMeta{
+						Name:              "test",
+						Finalizers:        []string{manifestWorkFinalizer},
+						DeletionTimestamp: &now,
+					},
+				},
+				&workv1.ManifestWork{
+					ObjectMeta: v1.ObjectMeta{
+						Name:      "test1",
+						Namespace: "test",
+					},
+				},
+				&workv1.ManifestWork{
+					ObjectMeta: v1.ObjectMeta{
+						Name:      "test-klusterlet-crds",
+						Namespace: "test",
+					},
+				},
+				&workv1.ManifestWork{
+					ObjectMeta: v1.ObjectMeta{
+						Name:      "test-klusterlet",
+						Namespace: "test",
+					},
+				},
+			},
+			request: reconcile.Request{
+				NamespacedName: types.NamespacedName{
+					Name: "test",
+				},
+			},
+			validateFunc: func(t *testing.T, runtimeClient client.Client) {
+				manifestWorks := &workv1.ManifestWorkList{}
+				if err := runtimeClient.List(context.TODO(), manifestWorks, &client.ListOptions{Namespace: "test"}); err != nil {
+					t.Errorf("unexpected error: %v", err)
+				}
+				if len(manifestWorks.Items) != 1 {
+					t.Errorf("expected one work, but failed %d", len(manifestWorks.Items))
+				}
+			},
+		},
+		{
+			name: "managed clusters is deleting - only has klusterlet",
+			startObjs: []runtime.Object{
+				&clusterv1.ManagedCluster{
+					ObjectMeta: v1.ObjectMeta{
+						Name:              "test",
+						Finalizers:        []string{manifestWorkFinalizer},
+						DeletionTimestamp: &now,
+					},
+				},
+				&workv1.ManifestWork{
+					ObjectMeta: v1.ObjectMeta{
+						Name:      "test-klusterlet",
+						Namespace: "test",
+					},
+				},
+			},
+			request: reconcile.Request{
+				NamespacedName: types.NamespacedName{
+					Name: "test",
+				},
+			},
+			validateFunc: func(t *testing.T, runtimeClient client.Client) {
+				manifestWorks := &workv1.ManifestWorkList{}
+				if err := runtimeClient.List(context.TODO(), manifestWorks, &client.ListOptions{Namespace: "test"}); err != nil {
+					t.Errorf("unexpected error: %v", err)
+				}
+				if len(manifestWorks.Items) != 1 {
+					t.Errorf("expected one work, but failed %d", len(manifestWorks.Items))
+				}
+			},
+		},
+		{
+			name: "managed clusters is deleting and managed clusters is offline",
+			startObjs: []runtime.Object{
+				&clusterv1.ManagedCluster{
+					ObjectMeta: v1.ObjectMeta{
+						Name:              "test",
+						Finalizers:        []string{manifestWorkFinalizer},
+						DeletionTimestamp: &now,
+					},
+					Status: clusterv1.ManagedClusterStatus{
+						Conditions: []v1.Condition{
+							{
+								Type:   clusterv1.ManagedClusterConditionAvailable,
+								Status: v1.ConditionFalse,
+							},
+						},
+					},
+				},
+				&workv1.ManifestWork{
+					ObjectMeta: v1.ObjectMeta{
+						Name:       "test",
+						Namespace:  "test",
+						Finalizers: []string{"test"},
+					},
+				},
+				&workv1.ManifestWork{
+					ObjectMeta: v1.ObjectMeta{
+						Name:       "test-crds",
+						Namespace:  "test",
+						Finalizers: []string{"test"},
+					},
+				},
+				&workv1.ManifestWork{
+					ObjectMeta: v1.ObjectMeta{
+						Name:       "test-klusterlet",
+						Namespace:  "test",
+						Finalizers: []string{"test"},
+					},
+				},
+			},
+			request: reconcile.Request{
+				NamespacedName: types.NamespacedName{
+					Name: "test",
+				},
+			},
+			validateFunc: func(t *testing.T, runtimeClient client.Client) {
+				manifestWorks := &workv1.ManifestWorkList{}
+				if err := runtimeClient.List(context.TODO(), manifestWorks, &client.ListOptions{Namespace: "test"}); err != nil {
+					t.Errorf("unexpected error: %v", err)
+				}
+				if len(manifestWorks.Items) != 0 {
+					t.Errorf("expected no works, but failed")
+				}
+			},
+		},
+		{
+			name: "apply klusterlet manifest works",
+			startObjs: []runtime.Object{
+				&clusterv1.ManagedCluster{
+					ObjectMeta: v1.ObjectMeta{
+						Name:       "test",
+						Finalizers: []string{manifestWorkFinalizer},
+					},
+					Status: clusterv1.ManagedClusterStatus{
+						Conditions: []v1.Condition{
+							{
+								Type:   clusterv1.ManagedClusterConditionJoined,
+								Status: v1.ConditionTrue,
+							},
+						},
+						Version: clusterv1.ManagedClusterVersion{Kubernetes: "v1.18.0"},
+					},
+				},
+				testinghelpers.GetImportSecret("test"),
+			},
+			request: reconcile.Request{
+				NamespacedName: types.NamespacedName{
+					Name: "test",
+				},
+			},
+			validateFunc: func(t *testing.T, runtimeClient client.Client) {},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			r := &ReconcileManifestWork{
+				clientHolder: &helpers.ClientHolder{
+					//RuntimeClient: fake.NewClientBuilder().WithScheme(testscheme).WithObjects(c.startObjs...).Build(),
+					RuntimeClient: fake.NewFakeClientWithScheme(testscheme, c.startObjs...),
+				},
+				scheme:   testscheme,
+				recorder: eventstesting.NewTestingEventRecorder(t),
+			}
+
+			_, err := r.Reconcile(c.request)
+			if err != nil {
+				t.Errorf("unexpected error: %v", err)
+			}
+
+			c.validateFunc(t, r.clientHolder.RuntimeClient)
+		})
+	}
+}
