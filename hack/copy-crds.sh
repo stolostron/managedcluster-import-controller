f8fae91c76100e79bd68d3bb4fe0638ff4949533
Jian Zhu <36154065+zhujian7@users.noreply.github.com>
2022-12-02 16:11:06 +0800

    Rollback the v1beta1 klusterlet CRD (#202)
    Signed-off-by: zhujian <jiazhu@redhat.com>
    
    Signed-off-by: zhujian <jiazhu@redhat.com>

--- hack/copy-crds.sh
+++ hack/copy-crds.sh
@@ -5,4 +5,9 @@ KLUSTERLET_CRD_V1_FILE="./vendor/open-cluster-management.io/api/operator/v1/0000
 KLUSTERLET_CRD_V1BETA1_FILE="./vendor/open-cluster-management.io/api/operator/v1/0001_00_operator.open-cluster-management.io_klusterlets.crd.yaml"
 
 cp $KLUSTERLET_CRD_V1_FILE ./pkg/controller/importconfig/manifests/klusterlet/crds/klusterlets.crd.v1.yaml
-cp $KLUSTERLET_CRD_V1BETA1_FILE ./pkg/controller/importconfig/manifests/klusterlet/crds/klusterlets.crd.v1beta1.yaml
+
+# The upstream v1beta1 klusterlet CRD is invalid, and we do not use the registraionConfiguration/workConfigration
+# feature in MCE, so stop update this CRD until the upstream has a fix.
+# See: https://github.com/open-cluster-management-io/api/issues/192
+#
+# cp $KLUSTERLET_CRD_V1BETA1_FILE ./pkg/controller/importconfig/manifests/klusterlet/crds/klusterlets.crd.v1beta1.yaml
