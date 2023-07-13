b7819a57c00217569a2b028d930c4a1ecc606806
xuezhaojun <zxue@redhat.com>
2023-07-13 22:20:30 +0800

    Revert "Remove: git pre commit. (#240)"
    This reverts commit f61c57e442c7c2a87165341853352e05abfeb0c7.

---
+++ build/before-make.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+###############################################################################
+# (c) Copyright IBM Corporation 2019, 2020. All Rights Reserved.
+# Note to U.S. Government Users Restricted Rights:
+# U.S. Government Users Restricted Rights - Use, duplication or disclosure restricted by GSA ADP Schedule
+# Contract with IBM Corp.
+# Copyright (c) Red Hat, Inc.
+# Copyright Contributors to the Open Cluster Management project
+###############################################################################
+
+ln -sf ../../build/git-hooks/pre-commit .git/hooks/pre-commit
+ln -sf ../../build/git-hooks/commit-msg .git/hooks/commit-msg
