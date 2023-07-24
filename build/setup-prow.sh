fc6c6ac4abbbf4c46c76c24bae101d1edefb2972
xuezhaojun <zxue@redhat.com>
2023-07-24 22:36:23 +0800

    Fix prow e2e fail issue. (#258)
    Signed-off-by: xuezhaojun <zxue@redhat.com>

--- build/setup-prow.sh
+++ build/setup-prow.sh
@@ -10,9 +10,13 @@ set -o nounset
 BUILD_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 REPO_DIR="$(dirname "$BUILD_DIR")"
 WORK_DIR="${REPO_DIR}/_output"
-
 mkdir -p "${WORK_DIR}"
 
+echo "###### prepare required crds"
+${KUBECTL} apply -f "$REPO_DIR/test/e2e/resources/hive"
+${KUBECTL} apply -f "$REPO_DIR/test/e2e/resources/assisted-service"
+
+echo "###### prepare required kubeconfigs"
 E2E_KUBECONFIG="${WORK_DIR}/e2e-kubeconfig"
 E2E_MANAGED_KUBECONFIG="${WORK_DIR}/e2e-managed-kubeconfig"
 
