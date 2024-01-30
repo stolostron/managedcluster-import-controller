809a95effacbb5faa959de84e885f9785ea7d17a
zhujian <jiazhu@redhat.com>
2024-01-30 07:06:54 +0000

    Update klusterlet crd
    Signed-off-by: zhujian <jiazhu@redhat.com>

--- hack/copy-crds.sh
+++ hack/copy-crds.sh
@@ -2,12 +2,16 @@
 # Copyright Contributors to the Open Cluster Management project
 
 KLUSTERLET_CRD_V1_FILE="./vendor/open-cluster-management.io/api/operator/v1/0000_00_operator.open-cluster-management.io_klusterlets.crd.yaml"
-KLUSTERLET_CRD_V1BETA1_FILE="./vendor/open-cluster-management.io/api/operator/v1/0001_00_operator.open-cluster-management.io_klusterlets.crd.yaml"
+KLUSTERLET_CRD_V1BETA1_FILE="./vendor/open-cluster-management.io/api/crdsv1beta1/0001_00_operator.open-cluster-management.io_klusterlets.crd.yaml"
 
-cp $KLUSTERLET_CRD_V1_FILE ./pkg/controller/importconfig/manifests/klusterlet/crds/klusterlets.crd.v1.yaml
+KLUSTERLET_CRD_V1_DEST_FILE="./pkg/bootstrap/manifests/klusterlet/crds/klusterlets.crd.v1.yaml"
+KLUSTERLET_CRD_V1BETA1_DEST_FILE="./pkg/bootstrap/manifests/klusterlet/crds/klusterlets.crd.v1beta1.yaml"
 
-# The upstream v1beta1 klusterlet CRD is invalid, and we do not use the registraionConfiguration/workConfigration
-# feature in MCE, so stop update this CRD until the upstream has a fix.
+cp $KLUSTERLET_CRD_V1_FILE $KLUSTERLET_CRD_V1_DEST_FILE
+
+cp $KLUSTERLET_CRD_V1BETA1_FILE $KLUSTERLET_CRD_V1BETA1_DEST_FILE
+# Remove lines containing "default: "
+# The upstream v1beta1 klusterlet CRD is invalid, remove the v1beta1 crd lines containing "default: " in MCE
+# until the upstream has a fix.
 # See: https://github.com/open-cluster-management-io/api/issues/192
-#
-# cp $KLUSTERLET_CRD_V1BETA1_FILE ./pkg/controller/importconfig/manifests/klusterlet/crds/klusterlets.crd.v1beta1.yaml
+sed -i '/default: /d' $KLUSTERLET_CRD_V1BETA1_DEST_FILE
