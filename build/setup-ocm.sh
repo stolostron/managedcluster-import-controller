a957e6ce5738bf8472f406d72afd62b8b0e4a663
Zhiwei Yin <zyin@redhat.com>
2025-01-09 15:48:59 +0800

    upgrade golang.org/x/crypto to 0.31.0 golang.org/x/net to 0.33.0
    Signed-off-by: Zhiwei Yin <zyin@redhat.com>

--- build/setup-ocm.sh
+++ build/setup-ocm.sh
@@ -10,7 +10,7 @@ set -o nounset
 # Input: KUBECTL(kubectl or oc), OCM_VERSION)
 
 KUBECTL=${KUBECTL:-kubectl}
-OCM_VERSION=${OCM_VERSION:-main}
+OCM_VERSION=backplane-2.5
 
 function wait_deployment() {
   set +e
@@ -37,7 +37,7 @@ rm -rf "$WORK_DIR/_repo_ocm"
 
 export OCM_BRANCH=$OCM_VERSION
 export IMAGE_NAME=quay.io/stolostron/registration-operator:$OCM_VERSION
-export REGISTRATION_OPERATOR_IMAGE=quay.io/stolostron/registration-operator:$OCM_VERSION
+export OPERATOR_IMAGE_NAME=quay.io/stolostron/registration-operator:$OCM_VERSION
 export REGISTRATION_IMAGE=quay.io/stolostron/registration:$OCM_VERSION
 export WORK_IMAGE=quay.io/stolostron/work:$OCM_VERSION
 export PLACEMENT_IMAGE=quay.io/stolostron/placement:$OCM_VERSION
