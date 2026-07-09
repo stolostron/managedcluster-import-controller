56d43b343e744c4145fda25d87f5a86c950606a8
Tesshu Flower <tflower@redhat.com>
2026-07-09 16:06:18 -0400

    build/setup-ocm.sh: address review feedback
    - Replace wait_deployment polling loop with kubectl wait --for=create
    - Extract diagnostics into debug_and_exit function
    - Use ::group::/::endgroup:: for collapsible GHA log sections
    - Use --ignore-not-found instead of || true where supported
    - Quote all shell variables
    - Use git -C instead of cd for sparse checkout
    
    Signed-off-by: Tesshu Flower <tflower@redhat.com>

--- build/setup-ocm.sh
+++ build/setup-ocm.sh
@@ -12,32 +12,20 @@ set -o nounset
 KUBECTL=${KUBECTL:-kubectl}
 OCM_VERSION=${OCM_VERSION:-main}
 
-function wait_deployment() {
-  set +e
-  for((i=0;i<60;i++));
-  do
-    ${KUBECTL} -n $1 get deploy $2
-    if [ 0 -eq $? ]; then
-      break
-    fi
-    echo "sleep 1 second to wait deployment $1/$2 to exist: $i"
-    sleep 1
-  done
-  set -e
-
-  if ! ${KUBECTL} -n $1 get deploy $2 &>/dev/null; then
-    echo "####### DIAGNOSTIC: deployment $1/$2 not found after 60s #######"
-    echo "=== Pods in namespace $1 ==="
-    ${KUBECTL} -n $1 get pods -o wide || true
-    echo "=== Pod details in namespace $1 ==="
-    ${KUBECTL} -n $1 describe pods || true
-    echo "=== Cluster Manager operator logs ==="
-    ${KUBECTL} -n open-cluster-management logs -l app=cluster-manager --tail=200 || true
-    echo "=== ClusterManager CR status ==="
-    ${KUBECTL} get clustermanagers -o yaml || true
-    echo "####### END DIAGNOSTIC #######"
-    exit 1
-  fi
+function debug_and_exit() {
+  echo "::group::####### DIAGNOSTIC: OCM setup failure #######"
+  echo "=== Pods in open-cluster-management ==="
+  ${KUBECTL} -n open-cluster-management get pods -o wide --ignore-not-found
+  echo "=== Pod details in open-cluster-management ==="
+  ${KUBECTL} -n open-cluster-management describe pods --ignore-not-found
+  echo "=== Pods in open-cluster-management-hub ==="
+  ${KUBECTL} -n open-cluster-management-hub get pods -o wide --ignore-not-found
+  echo "=== Cluster Manager operator logs ==="
+  ${KUBECTL} -n open-cluster-management logs -l app=cluster-manager --tail=200 || true
+  echo "=== ClusterManager CR status ==="
+  ${KUBECTL} get clustermanagers -o yaml --ignore-not-found
+  echo "::endgroup::"
+  exit 1
 }
 
 echo "###### deploy ocm"
@@ -52,24 +40,22 @@ HELM="${REPO_DIR}/_output/helm"
 # always from the same version.
 #
 OCM_CHART_DIR="${REPO_DIR}/_output/ocm"
-rm -rf ${OCM_CHART_DIR}
-git clone --depth 1 --branch $OCM_VERSION --filter=blob:none --sparse \
-  https://github.com/stolostron/ocm.git ${OCM_CHART_DIR}
-cd ${OCM_CHART_DIR}
-git sparse-checkout set deploy/cluster-manager/chart
-cd ${REPO_DIR}
+rm -rf "${OCM_CHART_DIR}"
+git clone --depth 1 --branch "$OCM_VERSION" --filter=blob:none --sparse \
+  https://github.com/stolostron/ocm.git "${OCM_CHART_DIR}"
+git -C "${OCM_CHART_DIR}" sparse-checkout set deploy/cluster-manager/chart
 
-${HELM} upgrade --install cluster-manager ${OCM_CHART_DIR}/deploy/cluster-manager/chart/cluster-manager \
---namespace=open-cluster-management --create-namespace --set replicaCount=1,images.registry="quay.io/stolostron",images.tag=$OCM_VERSION
+${HELM} upgrade --install cluster-manager "${OCM_CHART_DIR}/deploy/cluster-manager/chart/cluster-manager" \
+  --namespace=open-cluster-management --create-namespace \
+  --set replicaCount=1,images.registry="quay.io/stolostron",images.tag="$OCM_VERSION"
 
+${KUBECTL} wait -n open-cluster-management --for=create deployment/cluster-manager --timeout=60s || debug_and_exit
+${KUBECTL} -n open-cluster-management rollout status deployment/cluster-manager --timeout=120s || debug_and_exit
 
-wait_deployment open-cluster-management cluster-manager
-${KUBECTL} -n open-cluster-management rollout status deploy cluster-manager --timeout=120s
-
-wait_deployment open-cluster-management-hub cluster-manager-registration-controller
-${KUBECTL} -n open-cluster-management-hub rollout status deploy cluster-manager-registration-controller --timeout=120s
-${KUBECTL} -n open-cluster-management-hub rollout status deploy cluster-manager-registration-webhook --timeout=120s
-${KUBECTL} -n open-cluster-management-hub rollout status deploy cluster-manager-work-webhook --timeout=120s
+${KUBECTL} wait -n open-cluster-management-hub --for=create deployment/cluster-manager-registration-controller --timeout=60s || debug_and_exit
+${KUBECTL} -n open-cluster-management-hub rollout status deployment/cluster-manager-registration-controller --timeout=120s || debug_and_exit
+${KUBECTL} -n open-cluster-management-hub rollout status deployment/cluster-manager-registration-webhook --timeout=120s || debug_and_exit
+${KUBECTL} -n open-cluster-management-hub rollout status deployment/cluster-manager-work-webhook --timeout=120s || debug_and_exit
 
 # scale replicas to save resources, after the hub are installed, we don't need
 # the cluster-manager and placement-controller for the e2e test
