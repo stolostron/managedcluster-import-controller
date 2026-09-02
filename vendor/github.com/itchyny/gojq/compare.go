2ebf3ff823faf14ae6e948ed658e63371655ae82
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2026-09-02 15:22:54 -0400

    chore: bump ocm to v1.3.1 for NetworkPolicy (#1265)
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- vendor/github.com/itchyny/gojq/compare.go
+++ vendor/github.com/itchyny/gojq/compare.go
@@ -1,15 +1,21 @@
 package gojq
 
 import (
+	"cmp"
+	"encoding/json"
 	"math"
 	"math/big"
 )
 
-func compare(l, r interface{}) int {
+// Compare l and r, and returns jq-flavored comparison value.
+// The result will be 0 if l == r, -1 if l < r, and +1 if l > r.
+// This comparison is used by built-in operators and functions.
+func Compare(l, r any) int {
 	return binopTypeSwitch(l, r,
-		func(l, r int) interface{} {
+		cmp.Compare,
+		func(l, r float64) int {
 			switch {
-			case l < r:
+			case lt(l, r):
 				return -1
 			case l == r:
 				return 0
@@ -17,89 +23,54 @@ func compare(l, r interface{}) int {
 				return 1
 			}
 		},
-		func(l, r float64) interface{} {
-			switch {
-			case l < r || math.IsNaN(l):
-				return -1
-			case l == r:
-				return 0
-			default:
-				return 1
-			}
-		},
-		func(l, r *big.Int) interface{} {
-			return l.Cmp(r)
-		},
-		func(l, r string) interface{} {
-			switch {
-			case l < r:
-				return -1
-			case l == r:
-				return 0
-			default:
-				return 1
-			}
-		},
-		func(l, r []interface{}) interface{} {
-			for i := 0; ; i++ {
-				if i >= len(l) {
-					if i >= len(r) {
-						return 0
-					}
-					return -1
-				}
-				if i >= len(r) {
-					return 1
-				}
-				if cmp := compare(l[i], r[i]); cmp != 0 {
+		(*big.Int).Cmp,
+		cmp.Compare,
+		func(l, r []any) int {
+			for i := range min(len(l), len(r)) {
+				if cmp := Compare(l[i], r[i]); cmp != 0 {
 					return cmp
 				}
 			}
+			return cmp.Compare(len(l), len(r))
 		},
-		func(l, r map[string]interface{}) interface{} {
+		func(l, r map[string]any) int {
 			lk, rk := funcKeys(l), funcKeys(r)
-			if cmp := compare(lk, rk); cmp != 0 {
+			if cmp := Compare(lk, rk); cmp != 0 {
 				return cmp
 			}
-			for _, k := range lk.([]interface{}) {
-				if cmp := compare(l[k.(string)], r[k.(string)]); cmp != 0 {
+			for _, k := range lk.([]any) {
+				if cmp := Compare(l[k.(string)], r[k.(string)]); cmp != 0 {
 					return cmp
 				}
 			}
 			return 0
 		},
-		func(l, r interface{}) interface{} {
-			ln, rn := getTypeOrdNum(l), getTypeOrdNum(r)
-			switch {
-			case ln < rn:
-				return -1
-			case ln == rn:
-				return 0
-			default:
-				return 1
-			}
+		func(l, r any) int {
+			return cmp.Compare(typeIndex(l), typeIndex(r))
 		},
-	).(int)
+	)
 }
 
-func getTypeOrdNum(v interface{}) int {
+func lt(l, r float64) bool {
+	return l < r || math.IsNaN(l)
+}
+
+func typeIndex(v any) int {
 	switch v := v.(type) {
-	case nil:
+	default:
 		return 0
 	case bool:
-		if v {
-			return 2
+		if !v {
+			return 1
 		}
-		return 1
-	case int, float64, *big.Int:
+		return 2
+	case int, float64, *big.Int, json.Number:
 		return 3
 	case string:
 		return 4
-	case []interface{}:
+	case []any:
 		return 5
-	case map[string]interface{}:
+	case map[string]any:
 		return 6
-	default:
-		return -1
 	}
 }
