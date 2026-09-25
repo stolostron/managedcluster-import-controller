b3206d462bee2296fd53053d2714921c2f4af6c3
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2026-09-25 11:48:48 -0400

    chore: remove `flightctl` controller
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- vendor/github.com/flightctl/flightctl/lib/apipublic/v1alpha1/docs.go
+++
@@ -1,4 +0,0 @@
-package v1alpha1
-
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@v2.3.0 --config=types.gen.cfg openapi.yaml
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@v2.3.0  --config=spec.gen.cfg openapi.yaml
